7ce839961ec92b4c384212677973e41d8b360c03
unknown <rford@.aerosim.com>
2010-02-24 14:21:26 -0600

    Added an experimental operation.

--- test.cpp
+++ test.cpp
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+int TestOp( int p_iParam1, int p_iParam2 )
+{
+	return p_iParam1 | p_iParam;
+}
+
 int main()
 {
 	int i = 1; // I am adding a line now
 	printf( "This is a modified git test" );
+	printf( "Test operation: %d\n", TestOp( 42, 38 ) );
 	return 0;
 }
