f87c58a5661b627c5d0634e249b7c0b460d58877
unknown <rford@.aerosim.com>
2010-02-24 14:23:49 -0600

    Added an int list.

--- test.cpp
+++ test.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <vector>
 
 int main()
 {
+	std::vector<int> vIntList;
+	vIntList.clear();
 	int i = 1; // I am adding a line now
 	printf( "This is a modified git test" );
 	return 0;
